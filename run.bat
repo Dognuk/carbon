8584c87864e9d4201bd8d4cf81c3f896077abc46
jonathanredeker <jonathanredeker@live.com>
2019-03-19 07:58:51 -0700

    Changed javaw to java to enable console output

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-START javaw -Djava.library.path=lib/natives/windows -jar target\carbon-0.1-alpha-jar-with-dependencies.jar
-PAUSE
+START java -Djava.library.path=lib/natives/windows -jar target\carbon-0.1-alpha-jar-with-dependencies.jar
+::PAUSE
